70214b0fd3852ca13c0bd8159b47bae4e8cb18b3
Linh <dangvanlinh29022004@gmail.com>
2023-12-21 09:15:32 +0700

    updateee

--- scripts/payment.js
+++
@@ -1,9 +0,0 @@
-// Trong file payment.js
-$(document).ready(function() {
-    $('#submitOrder').click(function() {
-        // Xử lý logic đặt hàng ở đây
-
-        // Hiển thị thông báo chúc mừng
-        $('#successMessage').css('display', 'block');
-    });
-});
