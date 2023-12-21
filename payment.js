0c081699da650a1cbeeec4218f7705d5dd63edc4
Linh <dangvanlinh29022004@gmail.com>
2023-12-22 00:59:59 +0700

    uppp

---
+++ payment.js
@@ -0,0 +1,9 @@
+// Trong file payment.js
+$(document).ready(function() {
+    $('#submitOrder').click(function() {
+        // Xử lý logic đặt hàng ở đây
+
+        // Hiển thị thông báo chúc mừng
+        $('#successMessage').css('display', 'block');
+    });
+});
