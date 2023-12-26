6e2a2258ebf59849b2f2658f04c790127d1c30bf
Linh <dangvanlinh29022004@gmail.com>
2023-12-26 22:48:31 +0700

    uppp load file noww

--- script.js
+++ script.js
@@ -1,161 +1,178 @@
 
 $(document).ready(function () {
-    $("#searchButton").on("click", function () {
-      var searchTerm = $("#searchProduct").val().toLowerCase();
+  $("#searchButton").on("click", function () {
+    var searchTerm = $("#searchProduct").val().toLowerCase();
 
-      // Ẩn tất cả sản phẩm
-      $(".product-item").hide();
+    // Ẩn tất cả sản phẩm
+    $(".product-item").hide();
 
-      // Lặp qua từng sản phẩm và kiểm tra xem nó có chứa từ khóa tìm kiếm không
-      $(".product-item").each(function () {
-          var productName = $(this).find("h2").text().toLowerCase();
-          if (productName.includes(searchTerm)) {
-              // Nếu sản phẩm chứa từ khóa tìm kiếm, hiển thị nó
-              $(this).show();
-          }
-      });
-    });
-    // Danh sách sản phẩm đề xuất
-    var suggestedProducts = [
-        "Chocolate Oreo",
-        "Matcha Oreo",
-        "Trà Ổi Hồng",
-        // ... (thêm các sản phẩm khác)
-        "Yogurt xoài",
-        "Yogurt dâu"
-    ];
-    
-      // Sự kiện khi người dùng nhập từ khóa tìm kiếm
-    $("#searchProduct").on("input", function () {
-        var searchTerm = $(this).val().toLowerCase();
-        var suggestions = [];
-    
-        // Lọc sản phẩm dựa trên từ khóa tìm kiếm
-        for (var i = 0; i < suggestedProducts.length; i++) {
-          if (suggestedProducts[i].toLowerCase().includes(searchTerm)) {
-            suggestions.push(suggestedProducts[i]);
-          }
+    // Lặp qua từng sản phẩm và kiểm tra xem nó có chứa từ khóa tìm kiếm không
+    $(".product-item").each(function () {
+        var productName = $(this).find("h2").text().toLowerCase();
+        if (productName.includes(searchTerm)) {
+            // Nếu sản phẩm chứa từ khóa tìm kiếm, hiển thị nó
+            $(this).show();
         }
-    
-        // Hiển thị gợi ý
-        showSuggestions(suggestions);
     });
-    
-      // Hàm hiển thị gợi ý
-    function showSuggestions(suggestions) {
-        var searchResults = $("#searchResults");
-        searchResults.empty();
-    
-        // Hiển thị gợi ý trong một danh sách
-        for (var i = 0; i < suggestions.length; i++) {
-          var listItem = $("<li>").text(suggestions[i]);
-          searchResults.append(listItem);
+  });
+  // Danh sách sản phẩm đề xuất
+  var suggestedProducts = [
+      "Chocolate Oreo",
+      "Matcha Oreo",
+      "Trà Ổi Hồng",
+      // ... (thêm các sản phẩm khác)
+      "Yogurt xoài",
+      "Yogurt dâu"
+  ];
+  
+    // Sự kiện khi người dùng nhập từ khóa tìm kiếm
+  $("#searchProduct").on("input", function () {
+      var searchTerm = $(this).val().toLowerCase();
+      var suggestions = [];
+  
+      // Lọc sản phẩm dựa trên từ khóa tìm kiếm
+      for (var i = 0; i < suggestedProducts.length; i++) {
+        if (suggestedProducts[i].toLowerCase().includes(searchTerm)) {
+          suggestions.push(suggestedProducts[i]);
         }
-    
-        // Hiển thị danh sách gợi ý
-        if (suggestions.length > 0) {
-          searchResults.show();
-        } else {
-          searchResults.hide();
-        }
-    }
-    
-      // Sự kiện khi người dùng chọn một gợi ý
-    $("#searchResults").on("click", "li", function () {
-        var selectedProduct = $(this).text();
-        $("#searchProduct").val(selectedProduct);
-        $("#searchResults").hide();
-    });
-
-    // Hàm để thêm sản phẩm vào giỏ hàng
-    $(".order-btn").on("click", function () {
-      var product = $(this).closest(".product-item");
-      var productName = product.find("h2").text();
-      var quantity = parseInt(product.find(".quantity").val());
-      var size = product.find(".size").val();
-      var price = (size === "M") ? 22000 : 27000;
-      var total = quantity * price;
-  
-      addToCart(productName, quantity, total);
-    });
-  
-    // Hàm để hiển thị phần viết đánh giá khi nhấn nút
-    $(".review-btn").on("click", function () {
-      var product = $(this).closest(".product-item");
-      var reviewTextarea = product.find(".review-textarea");
-      var submitReviewBtn = product.find(".submit-review-btn");
-      var thankYouMessage = product.find(".review-thank-you");
-  
-      reviewTextarea.show();
-      submitReviewBtn.show();
-      thankYouMessage.hide();
-    });
-  
-    // Hàm để gửi đánh giá và hiển thị thông báo cảm ơn
-    $(".submit-review-btn").on("click", function () {
-      var product = $(this).closest(".product-item");
-      var reviewTextarea = product.find(".review-textarea");
-      var submitReviewBtn = product.find(".submit-review-btn");
-      var thankYouMessage = product.find(".review-thank-you");
-  
-      if (reviewTextarea.val().trim() !== "") {
-        thankYouMessage.show();
-        reviewTextarea.hide();
-        submitReviewBtn.hide(); // Ẩn nút Gửi Đánh Giá
-      } else {
-        alert("Vui lòng viết đánh giá trước khi gửi.");
       }
-    });
   
-    // Hàm xử lý khi nhấn nút thanh toán
-    $("#paymentButton").on("click", function () {
-      // Chuyển hướng đến trang payment.html
-      window.location.href = "payment.html";
-    });
+      // Hiển thị gợi ý
+      showSuggestions(suggestions);
+  });
   
-    // Hàm hỗ trợ để thêm sản phẩm vào giỏ hàng
-    function addToCart(productName, quantity, total) {
-      var cartList = $("#cartList");
-      var cartItem = $("<li>").text(`${productName} - Số lượng: ${quantity} - Tổng cộng: ${total} đ`);
-      cartList.append(cartItem);
+    // Hàm hiển thị gợi ý
+  function showSuggestions(suggestions) {
+      var searchResults = $("#searchResults");
+      searchResults.empty();
   
-      // Cập nhật tổng cộng giỏ hàng
-      updateCartSummary();
+      // Hiển thị gợi ý trong một danh sách
+      for (var i = 0; i < suggestions.length; i++) {
+        var listItem = $("<li>").text(suggestions[i]);
+        searchResults.append(listItem);
+      }
+  
+      // Hiển thị danh sách gợi ý
+      if (suggestions.length > 0) {
+        searchResults.show();
+      } else {
+        searchResults.hide();
+      }
+  }
+  
+    // Sự kiện khi người dùng chọn một gợi ý
+  $("#searchResults").on("click", "li", function () {
+      var selectedProduct = $(this).text();
+      $("#searchProduct").val(selectedProduct);
+      $("#searchResults").hide();
+  });
+
+  function addToCart(productId, productName, size, price, quantity) {
+    // Lấy danh sách sản phẩm hiện tại từ localStorage hoặc tạo một mảng trống nếu chưa có
+    const cartItems = JSON.parse(localStorage.getItem('cartItems')) || [];
+
+    // Kiểm tra xem sản phẩm đã có trong giỏ hàng hay chưa dựa trên productId và size
+    const existingItemIndex = cartItems.findIndex(item => item.productId === productId && item.size === size);
+
+    if (existingItemIndex !== -1) {
+        // Nếu sản phẩm đã tồn tại, cập nhật số lượng
+        cartItems[existingItemIndex].quantity += quantity;
+    } else {
+        // Nếu sản phẩm chưa có trong giỏ hàng, thêm vào
+        cartItems.push({
+            productId,
+            productName,
+            size,
+            price,
+            quantity
+        });
     }
-  
-    // Hàm hỗ trợ để cập nhật tổng cộng giỏ hàng
-    function updateCartSummary() {
-      var cartList = $("#cartList");
-      var cartSummary = $("#cartSummary");
-      var totalAmount = 0;
-  
-      // Tính tổng cộng
-      cartList.find("li").each(function () {
-        var text = $(this).text();
-        var total = parseInt(text.match(/Tổng cộng: (\d+)/)[1]);
-        totalAmount += total;
+
+    // Lưu danh sách sản phẩm đã cập nhật vào localStorage
+    localStorage.setItem('cartItems', JSON.stringify(cartItems));
+  }
+
+// Bộ lắng nghe sự kiện cho nút "Thêm vào giỏ hàng"
+  document.querySelectorAll('.order-btn').forEach((button, index) => {
+      button.addEventListener('click', () => {
+          const product = document.querySelectorAll('.product')[index];
+          const productId = product.id;
+          const productName = product.querySelector('h2').innerText;
+          const size = product.querySelector('.size').value;
+          const price = parseFloat(product.querySelector('.size').options[product.querySelector('.size').selectedIndex].innerText.replace(/\D/g, ''));
+          const quantity = parseInt(product.querySelector('.quantity').value);
+
+          // Thêm sản phẩm vào giỏ hàng
+          addToCart(productId, productName, size, price, quantity);
       });
-  
-      // Cập nhật thông tin tổng cộng giỏ hàng
-      cartSummary.text(`Tổng cộng: ${totalAmount} đ`);
+  });
+
+  // Thêm hàm showNotification vào mã JavaScript của bạn
+function showNotification(message) {
+  // Tìm phần tử thông báo theo ID
+  var notification = $("#notification");
+
+  // Đặt nội dung thông báo
+  notification.text(message);
+
+  // Hiển thị thông báo
+  notification.fadeIn().delay(2000).fadeOut(); // Hiển thị trong 2 giây, sau đó ẩn đi
+}
+
+// Bộ lắng nghe sự kiện cho nút "Thêm vào giỏ hàng"
+  $(".order-btn").on("click", function () {
+    const product = $(this).closest(".product-item");
+    const productName = product.find("h2").text();
+
+    // Gọi hàm showNotification với nội dung thông báo
+    showNotification(productName + " đã được thêm vào giỏ hàng!");
+  });
+
+  // Hàm để hiển thị phần viết đánh giá khi nhấn nút
+  $(".review-btn").on("click", function () {
+    var product = $(this).closest(".product-item");
+    var reviewTextarea = product.find(".review-textarea");
+    var submitReviewBtn = product.find(".submit-review-btn");
+    var thankYouMessage = product.find(".review-thank-you");
+
+    reviewTextarea.show();
+    submitReviewBtn.show();
+    thankYouMessage.hide();
+  });
+
+  // Hàm để gửi đánh giá và hiển thị thông báo cảm ơn
+  $(".submit-review-btn").on("click", function () {
+    var product = $(this).closest(".product-item");
+    var reviewTextarea = product.find(".review-textarea");
+    var submitReviewBtn = product.find(".submit-review-btn");
+    var thankYouMessage = product.find(".review-thank-you");
+
+    if (reviewTextarea.val().trim() !== "") {
+      thankYouMessage.show();
+      reviewTextarea.hide();
+      submitReviewBtn.hide(); // Ẩn nút Gửi Đánh Giá
+    } else {
+      alert("Vui lòng viết đánh giá trước khi gửi.");
     }
   });
 
-document.addEventListener("DOMContentLoaded", function () {
+  function redirectToPayment() {
+    window.location.href = 'payment.html';
+  }
+  
+  function removeItem(button) {
+    const row = button.parentNode.parentNode;
+    row.parentNode.removeChild(row);
+  }
+  
+  // Gọi hàm để hiển thị sản phẩm ban đầu
+  renderCart();
+
+  document.addEventListener("DOMContentLoaded", function () {
     var logoContainer = document.getElementById("logo-container");
     var cartContainer = document.getElementById("cart");
-  
+
     var fixedClass = "fixed"; // Tên lớp CSS cho vị trí cố định
-  
-    window.addEventListener("scroll", function () {
-      if (window.scrollY > 100) {
-        // Thêm lớp cố định khi người dùng cuộn trang xuống
-        logoContainer.classList.add(fixedClass);
-        cartContainer.classList.add(fixedClass);
-      } else {
-        // Xóa lớp cố định khi người dùng cuộn trang lên đầu
-        logoContainer.classList.remove(fixedClass);
-        cartContainer.classList.remove(fixedClass);
-      }
-    });
+
   });
+});
